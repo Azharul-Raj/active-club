46e0cebe7d4fc5786c6a49131fbb80938efb915f
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-29 02:40:16 +0600

    first question added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Workouts from './Components/Workouts/Workouts';
+import Question from './Components/Question/Question';
 
 function App() {
   return (
     <div className="App">
-      <Workouts/>
+      <Workouts />
+      <Question/>
     </div>
   );
 }
